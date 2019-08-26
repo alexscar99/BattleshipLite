7c26d80c362bb7127128d473702e56bbeae22345
Alex Scarlett <alex.scarlett91@gmail.com>
2019-08-26 15:32:18 -0400

    Fixed equality check with strings to use best C# practices. Extracted repeated logic of checking for matching row and column to bool helper method

--- BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
+++ BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
@@ -110,31 +110,40 @@ namespace BattleshipLiteLibrary
 
         public static (string row, int column) SplitShotIntoRowAndColumn(string shot)
         {
+            if (shot.Length != 2)
+            {
+                return ("", 0);
+            }
+
             return (shot.Substring(0, 1), Convert.ToInt32(shot.Substring(1)));
         }
 
         public static bool ValidateShot(PlayerInfoModel activePlayer, string row, int column)
         {
-            bool isValidRow = (row.ToLower() == "a") || (row.ToLower() == "b") || (row.ToLower() == "c") || 
-                (row.ToLower() == "d") || (row.ToLower() == "e");
-            bool isValidColumn = (column > 0) && (column < 6);
+            var validRows = new List<string> { "a", "b", "c", "d", "e" };
+            bool isValidRow = validRows.Contains(row, StringComparer.OrdinalIgnoreCase);
+            bool isValidColumn = column > 0 && column < 6;
+            bool isValidShot = false;
 
             foreach (var gridSpot in activePlayer.ShotGrid)
             {
-                if (isValidRow && isValidColumn && (gridSpot.Status != GridSpotStatus.Hit || gridSpot.Status != GridSpotStatus.Miss))
+                if (isValidRow && isValidColumn)
                 {
-                    return true;
+                    if (IsMatchingRowAndColumn(gridSpot, row, column))
+                    {
+                        isValidShot = (gridSpot.Status == GridSpotStatus.Empty) ? true : false;
+                    }
                 }
             }
 
-            return false;
+            return isValidShot;
         }
 
         public static bool IdentifyShotResult(PlayerInfoModel opponent, string row, int column)
         {
             foreach (var ship in opponent.ShipLocations)
             {
-                if (ship.SpotLetter.ToLower() == row.ToLower() && ship.SpotNumber == column)
+                if (IsMatchingRowAndColumn(ship, row, column))
                 {
                     ship.Status = GridSpotStatus.Sunk;
                     return true;
@@ -148,11 +157,21 @@ namespace BattleshipLiteLibrary
         {
             foreach (var spot in activePlayer.ShotGrid)
             {
-                if (spot.SpotLetter.ToLower() == row.ToLower() && spot.SpotNumber == column)
+                if (IsMatchingRowAndColumn(spot, row, column))
                 {
                     spot.Status = isAHit ? GridSpotStatus.Hit : GridSpotStatus.Miss;
                 }
             }
+        }
+
+        private static bool IsMatchingRowAndColumn(GridSpotModel gridSpot, string row, int column)
+        {
+            if (gridSpot.SpotLetter.Equals(row, StringComparison.OrdinalIgnoreCase) && gridSpot.SpotNumber == column)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
