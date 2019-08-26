c34a31714738de32e4a8ccafc193f2488622e1cf
Alex Scarlett <alex.scarlett91@gmail.com>
2019-08-25 20:56:27 -0400

    Added GameLogic class to the class library for handling the logic of the game
    Started first half of the UI design by implementing methods for welcome msg, acquiring the player's name, initializing the grid, and placing the ships

--- BattleshipLiteApp/BattleshipLiteLibrary/Models/Enums.cs
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/Enums.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace BattleshipLiteLibrary.Models
 {
-    // 0 = empty, 1 = ship, 2 = miss, 3 = hit, 4 = sunk
     public enum GridSpotStatus
     {
         Empty,
